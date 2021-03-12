e666396be9525d1e1fb4fddcd638f4071c2fb70b
Nemtullo <koznem24@gmail.com>
2021-03-12 16:07:04 +0100

    Completing assignment.

---
+++ src/main/java/com/koznem/recipe/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.koznem.recipe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        System.out.println("Hello world");
+        return "index";
+    }
+}
