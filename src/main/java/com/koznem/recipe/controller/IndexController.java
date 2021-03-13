08fb732b19f48234bd8513d537bc34a319686ad8
Nemtullo <koznem24@gmail.com>
2021-03-13 20:08:36 +0100

    Jpa Pro-tips Implementation.

--- src/main/java/com/koznem/recipe/controller/IndexController.java
+++ src/main/java/com/koznem/recipe/controller/IndexController.java
@@ -1,14 +1,23 @@
 package com.koznem.recipe.controller;
 
+import com.koznem.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
-        System.out.println("Hello world");
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes",recipeService.getRecipes());
         return "index";
     }
 }
---
+++ src/main/java/com/koznem/recipe/services/RecipeService.java
@@ -0,0 +1,9 @@
+package com.koznem.recipe.services;
+
+import com.koznem.recipe.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+}
