1caac56de8a862026b2f3d782e701d96929fafbd
Nemtullo <koznem24@gmail.com>
2021-04-06 09:36:38 +0200

    Added Recipe Controller and Test classes

---
+++ src/main/java/com/koznem/recipe/controller/RecipeController.java
@@ -0,0 +1,25 @@
+package com.koznem.recipe.controller;
+
+import com.koznem.recipe.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RecipeController {
+
+    RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+
+        model.addAttribute("recipe",recipeService.findById(Long.valueOf(id)));
+
+        return "recipe/show";
+    }
+}
